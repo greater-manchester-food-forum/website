8451b64a3d4144409b26e5c9ce8108362e15af39
Reece Johnson <reece.johnson@thoughtworks.com>
2019-01-21 12:42:42 +0000

    [Reece/Flinn] Our mission page mock up with hover images

--- src/components/OurMission/Mission.js
+++ src/components/OurMission/Mission.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 export default class Mission extends Component {
   state = {
@@ -6,27 +7,35 @@ export default class Mission extends Component {
   };
 
   toggleBackground = shown => {
+    console.log(`background show = ${shown}`);
     this.setState({ backgroundShown: shown });
   };
 
   render() {
     const { backgroundShown } = this.state;
     const { title, image, desc } = this.props;
+    const bgPosition = backgroundShown ? 'center' : '-700px';
+    const textColour = backgroundShown ? 'text-white' : 'text-safe-green';
     return (
       <div
-        className="border border-grey p-12 py-16 mission-container bg-center bg-no-repeat bg-cover"
-        style={
-          backgroundShown
-            ? {
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${image})`,
-              }
-            : {}
-        }
+        className={`flex flex-col justify-between border border-grey p-12 py-16 mission-container bg-no-repeat bg-cover ${textColour}`}
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${image})`,
+          backgroundPosition: bgPosition,
+        }}
         onMouseEnter={() => this.toggleBackground(true)}
         onMouseLeave={() => this.toggleBackground(false)}
       >
         <h2 className="text-3xl">{title}</h2>
-        <p />
+        <div className="bg-safe-green-dark h-1 w-12 " />
+        <p
+          className={classNames({
+            'text-white': backgroundShown,
+            'text-black': !backgroundShown,
+          })}
+        >
+          {desc}
+        </p>
       </div>
     );
   }
--- src/components/OurMission/OurMission.js
+++ src/components/OurMission/OurMission.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import Mission from './Mission';
-import collab from '../../images/collab.jpeg';
+import collab from '../../images/collaboration.jpg';
+import foodAccess from '../../images/food-access.jpg';
+import healthyFood from '../../images/healthy-food.jpg';
+import localBusiness from '../../images/local-business.jpg';
 
 function OurMission() {
   return (
     <React.Fragment>
       <div className="py-20 container mx-auto" id="mission-statement">
-        <h2 className="text-5xl font-bold tracking-tight leading-tight text-safe-green text-center mb-12">
+        <h2 className="text-5xl font-bold tracking-tight leading-tight text-safe-green-darker text-center mb-12">
           Our Mission
         </h2>
-        <div className="mission-grid">
-          <Mission title="Collaboration" image={collab} desc="" />
-          <Mission title="Food Access" image={collab} desc="" />
-          <Mission title="Local Business" image={collab} desc="" />
-          <Mission title="Health" image={collab} desc="" />
+        <div className="mission-grid px-6 md:px-0">
+          <Mission
+            title="Collaboration"
+            image={collab}
+            desc="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+          />
+          <Mission
+            title="Food Access"
+            image={foodAccess}
+            desc="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint."
+          />
+          <Mission
+            title="Local Business"
+            image={localBusiness}
+            desc="Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas."
+          />
+          <Mission
+            title="Health"
+            image={healthyFood}
+            desc="Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+          />
         </div>
+        <button
+          className="btn btn-green mt-3 mx-auto text-center"
+          type="button"
+        >
+          Read our full mission statement
+        </button>
       </div>
     </React.Fragment>
   );
