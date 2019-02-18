9dde7ccbf5a3c5ff38966173670eb8e5d08b0c3b
Ryan Brooks <ryan@slatehorse.com>
2019-02-18 19:05:00 +0000

    collaborator updates (added GMFPA) for Charlie

--- src/collaborators.js
+++ src/collaborators.js
@@ -3,29 +3,29 @@ import goodGM from './images/good-food-for-gm.png';
 import GMCA from './images/gcma.png';
 import realFoodGuide from './images/real-food-guide.png';
 import openKitch from './images/open-kitch.png';
-import kindTrust from './images/kind-trust.png';
+import gmfpaLogo from './images/GMPA-GMFPA.jpg';
 import emerge from './images/emerge-logo.png';
 
 const collaborators = [
-  {
-    name: 'Feeding GM',
-    logo: feedingGM,
-    url: 'https://feedinggtrmcr.org.uk/',
-    mainCollaborator: true,
-  },
   {
     name: 'Good Food For GM',
     logo: goodGM,
     url: 'https://goodfoodgreaterman.wixsite.com/home',
     mainCollaborator: true,
   },
+  {
+    name: 'Feeding GM',
+    logo: feedingGM,
+    url: 'https://feedinggtrmcr.org.uk/',
+    mainCollaborator: true,
+  },
   { name: 'GMCA’s Health Hub', logo: GMCA },
   { name: 'Friends of the Earth', logo: realFoodGuide },
   { name: 'Real Junk Food Mcr', logo: openKitch },
   {
-    name: 'Kindling Trust',
-    logo: kindTrust,
-    url: 'https://kindling.org.uk/',
+    name: 'Greater Manchester Food Poverty Alliance',
+    logo: gmfpaLogo,
+    url: 'https://www.sustainweb.org/foodpower/map/greater_manchester_food_poverty_alliance/',
     mainCollaborator: true,
   },
   { name: 'FareShare GM', logo: emerge },
